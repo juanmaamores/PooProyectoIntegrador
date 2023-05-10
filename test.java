c8019b883884c2de38e3db6a2c6109cc17f120b1
Juan <amoresdeveloper@gmail.com>
2023-05-10 17:02:49 -0300

    Agregué un hola mundo 4

--- test.java
+++ test.java
@@ -3,5 +3,6 @@ public class HolaMundo {
         System.out.println("¡Hola mundo!");
         System.out.println("¡Hola mundo 2!");
         System.out.println("¡Hola mundo 3!");
+        System.out.println("¡Hola mundo 4!");
     }
 }
