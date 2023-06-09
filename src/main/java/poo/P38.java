0831a4bfc2b0190c15a441604beb33e16747268b
Martin <martindalsanto38@gmail.com>
2023-06-08 23:45:16 -0300

    - Se pasaron los cambios hechos a P38 en Enemigos

--- src/main/java/poo/P38.java
+++ src/main/java/poo/P38.java
@@ -1,22 +1,37 @@
 package poo;
 
 import poo.Interfaces.Disparable;
+import poo.Interfaces.Movil;
 import poo.Sistema.Cronometro;
 
 import java.awt.image.BufferedImage;
 
-public class P38 extends ObjetoGrafico implements Disparable{
+public class P38 extends ObjetoGrafico implements Disparable, Movil {
     private static int energia, cantAtaqEsp;
     private int velocidadDeDisparoP38 = 300;
     private Cronometro velocidadDeDisparo;
 
-    public P38(double v, double v1, BufferedImage imgHeroe) {
-        super(v,v1,imgHeroe);
+    public P38(){
+        setImagen(Utilidades.getImagenP38(0));
         energia = 100;
         cantAtaqEsp = 3;
         energia = energia - 20;
         velocidadDeDisparo = new Cronometro();
     }
+
+    public void moverse(int ancho, int alto){
+        if(x <= 5)
+            x = 5;
+
+        if(x >= ancho-35)
+            x = ancho-35;
+
+        if(y <= alto-585)
+            y = alto-585;
+
+        if(y >= alto-30)
+            y = alto-30;
+    };
 
     public static void setEnergia(int e){
         if(energia + e <= 100){
@@ -40,6 +55,8 @@ public class P38 extends ObjetoGrafico implements Disparable{
         velocidadDeDisparo.run(velocidadDeDisparoP38);
     }
 
+    public Municion disparar(){return new Municion();}; //se puso este return para que no de error
+
     public boolean getVelocidadDeDisparo(){
         return velocidadDeDisparo.isRunning();
     }
@@ -49,3 +66,8 @@ public class P38 extends ObjetoGrafico implements Disparable{
     }
 
 }
+
+
+
+
+
