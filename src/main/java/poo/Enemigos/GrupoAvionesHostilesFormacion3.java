82bc220dda12cc6df716db56c205b556ef911976
Martin <martindalsanto38@gmail.com>
2023-06-08 23:56:57 -0300

    - Se agregó la clase GrupoAvionesHostilesFormacion3

---
+++ src/main/java/poo/Enemigos/GrupoAvionesHostilesFormacion3.java
@@ -0,0 +1,21 @@
+package poo.Enemigos;
+
+import java.util.Vector;
+
+import static java.lang.Math.abs;
+
+public class GrupoAvionesHostilesFormacion3 extends GrupoAvionesHostiles{
+    public GrupoAvionesHostilesFormacion3(int alto){
+        aviones = new Vector<>();
+        int pos = 0;
+        for(int i=0; i <= 4; i++) {
+            aviones.add(new AvionHostil());
+            aviones.get(i).setVelocidadH(0);
+            aviones.get(i).setVelocidadV(2);
+            aviones.get(i).setPosicion(370+pos,(alto-805)-(abs(pos)));
+            if((i%2)==0)
+                pos += 80;
+            pos *= -1;
+        }
+    }
+}
